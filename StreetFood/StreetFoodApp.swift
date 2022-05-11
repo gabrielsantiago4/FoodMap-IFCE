cdd7f032c31ce2c13646b8820d34c44695686c18
Gabriel Santiago <gaaasts@gmail.com>
2022-05-11 09:02:37 -0300

    Initial Commit

---
+++ StreetFood/StreetFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreetFoodApp.swift
+//  StreetFood
+//
+//  Created by Gabriel Santiago on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct StreetFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
